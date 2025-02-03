bd99537bd09b85ed653904d5b6089997516fb1ef
Preethii364 <preethiiganesh2005@gmail.com>
2025-02-03 15:01:13 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,6 +1,6 @@
 # Store input numbers
-num1 = input('Enter first number: ')
-num2 = input( 'Enter second number: ')
+num1 = input('Enter the value for first number: ')
+num2 = input( 'Enter the value for second number: ')
 
 # Add two numbers
 sum = float(num1 + float )
